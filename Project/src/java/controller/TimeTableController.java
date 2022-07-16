ad0213ba5fbf048afa496296054f3e6c454b2ffd
Hungtquoc <89761547+Hungtquoc@users.noreply.github.com>
2022-07-16 21:47:24 +0700

    Update TimeTableController.java

--- Project/src/java/controller/TimeTableController.java
+++ Project/src/java/controller/TimeTableController.java
@@ -25,6 +25,8 @@ import model.Session;
 import org.apache.tomcat.util.digester.ArrayStack;
 import model.Week;
 import dal.SlotTime;
+import java.time.format.DateTimeFormatter;
+import model.lecture;
 /**
  *
  * @author trnha
@@ -58,7 +60,7 @@ public class TimeTableController extends HttpServlet {
         Week currentWeek = getWeekByDate(weeks, currentDate);
         SessionDBContext sDBC = new SessionDBContext();
         int lectureId = 5;
-        ArrayList<Session> sessions= sDBC.getFromToDate(lectureId, currentWeek.getStartDate(), currentWeek.getEndDate())
+        ArrayList<Session> sessions= sDBC.getFromToDate(lectureId, currentWeek.getStartDate(), currentWeek.getEndDate());
         request.setAttribute("sessions", sessions);
         request.setAttribute("week", currentWeek);
         request.setAttribute("slot", slot.list());
@@ -69,7 +71,7 @@ public class TimeTableController extends HttpServlet {
         request.getRequestDispatcher("view/Calendar/calendar.jsp").forward(request, response);
     }
 
-    @Override
+
     public ArrayList<Week> getWeeksOfYear() {
         ArrayList<Week> weeks = new ArrayList<>();
         LocalDate startDate = LocalDate.parse("03-01-2022", DateTimeFormatter.ofPattern("dd-MM-yyyy"));
@@ -98,6 +100,23 @@ public class TimeTableController extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        ArrayList<Week> weeks = getWeeksOfYear();
+        int index = Integer.parseInt(request.getParameter("week_index"));
+        Week w = weeks.get(index);
+        LocalDate currentDate = w.getStartDate();
+        Week currentWeek = getWeekByDate(weeks, currentDate);
+        SessionDBContext sDB= new SessionDBContext();
+        SlotTime slot= new SlotTime(); 
+        //get session
+        int lec = 5;
+        ArrayList<Session> sessions = sDB.getFromToDate(lec, currentWeek.getStartDate(), currentWeek.getEndDate());
+        //set attributes
+        request.setAttribute("sessions", sessions);
+        request.setAttribute("slots", slot.list());
+        request.setAttribute("week", currentWeek);
+        request.setAttribute("date", currentDate);
+        request.setAttribute("weeks", weeks);
+        request.getRequestDispatcher("view/schedule.jsp").forward(request, response);
     }
 
     @Override
